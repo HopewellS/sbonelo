94723083cd382944c370cc674a6b558ebde1991c
HopewellS <43682071+HopewellS@users.noreply.github.com>
2018-09-28 20:58:46 +0200

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,7 +1,8 @@
 <?PHP
 echo "hello world"
-
-
+If($age<18){echo 'child';
+//display child if age is less than 18
+           }else {echo 'adult';}//display adult if age is greater than or equals 18
 
 
 
